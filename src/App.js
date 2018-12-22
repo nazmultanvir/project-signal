c0b24ad3675170b500f0fdcc15eebeb07cf092d7
Nazmul Tanvir <nazmul_tanvir@yahoo.com>
2018-12-22 19:43:32 +0600

    updated ws link

--- src/App.js
+++ src/App.js
@@ -12,12 +12,11 @@ import mqtt from "mqtt";
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { signalLeft: red, signalTop : red, signalRight: red,signalBottom: green, btn:false, btn1:false, btn2:true, btn3:false };
-    this.changeSignalData = this.changeSignalData.bind(this); 
+    this.state = { signalLeft: red, signalTop : red, signalRight: red,signalBottom: green, btn:"red", btn1:'red', btn2:'green', btn3:'red' };
   }
   
   componentWillMount(){
-    var client  = mqtt.connect("ws://iot.eclipse.org:80/ws",{clientId:"mqttjs01"});
+    var client  = mqtt.connect("ws://192.168.0.12:9001",{clientId:"mqttjs01"});
     client.on("connect",()=>console.log("connected !"));
     client.on('connect', function () {
       client.subscribe('fromNode1', function (err) {
@@ -27,6 +26,9 @@ class App extends Component {
         }
       })
     })
+
+
+
     client.on('message', (topic, message)=> {
       console.log(message, topic);
 
@@ -53,7 +55,7 @@ class App extends Component {
       }
       let data = message.toString();
       let lightSignal = data.split(",");
-      //console.log(lightSignal);
+      
 
 
       this.setState({btn3: signalConvterButton(lightSignal[0], lightSignal[1], lightSignal[2]), btn : signalConvterButton(lightSignal[3], lightSignal[4], lightSignal[5]), btn1: signalConvterButton(lightSignal[6], lightSignal[7], lightSignal[8]),btn2: signalConvterButton(lightSignal[9], lightSignal[10], lightSignal[11])});
@@ -61,20 +63,23 @@ class App extends Component {
     })
   }
 
+
   changeSignalData(data){
-    var client  = mqtt.connect("ws://iot.eclipse.org:80/ws",{clientId:"mqttjs01"});
+    var client  = mqtt.connect("ws://192.168.0.12:9001",{clientId:"mqttjs01"});
     if(data==1){
-      client.publish('fromCentralServer', '0,0,1,1,0,0,1,0,0,1,0,0');
+      client.publish('fromCentralServer', '001100100100');
     }else if(data==2){
-      client.publish('fromCentralServer', '1,0,0,0,0,1,1,0,0,1,0,0');
+      client.publish('fromCentralServer', '100001100100');
     }else if(data==3){
-      client.publish('fromCentralServer', '1,0,0,1,0,0,0,0,1,1,0,0');
+      client.publish('fromCentralServer', '100100001100');
     }else if(data==4){
-      client.publish('fromCentralServer', '1,0,0,1,0,0,1,0,0,0,0,1');
+      client.publish('fromCentralServer', '100100100001');
     }else{
-      client.publish('fromCentralServer', '0,0,0,0,0,0,0,0,0,0,0,0'); 
+      client.publish('fromCentralServer', '000000000000'); 
     }
   }
+
+
   render() {
 
     return (
@@ -103,7 +108,7 @@ class App extends Component {
               <button className="signalControllerButton" style={{background: this.state.btn}} onClick={()=>{this.changeSignalData(1)}}>ROAD 1</button>
               <button className="signalControllerButton" style={{background: this.state.btn1}} onClick={()=>{this.changeSignalData(2)}}>ROAD 2</button>
               <button className="signalControllerButton" style={{background: this.state.btn2}} onClick={()=>{this.changeSignalData(3)}}>ROAD 3</button>
-              <button className="signalControllerButton" style={{background: this.state.btn3}} onClick={()=>{this.changeSignalData(4)}}>ROAD 5</button>
+              <button className="signalControllerButton" style={{background: this.state.btn3}} onClick={()=>{this.changeSignalData(4)}}>ROAD 4</button>
             </div>
 
         </div>
